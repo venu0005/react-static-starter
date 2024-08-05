0b4b35e9f6589d56b5f8bb94abffa290e6824c09
Abhijith <venu0005@algonquinlive.com>
2024-08-05 19:41:22 -0400

    d

--- src/templates/HobbyTemplate.js
+++ src/templates/HobbyTemplate.js
@@ -3,11 +3,11 @@ import './HobbyTemplate.css'; // Ensure to import your CSS file
 
 const HobbyTemplate = ({ children }) => (
   <div className="hobby-template">
-    <header className="hobby-header">Hobby Template Header</header>
+    <header className="hobby-header">MY Hobbies</header>
     <main className="hobby-content">
       {children}
     </main>
-    <footer className="hobby-footer">Hobby Template Footer</footer>
+    <footer className="hobby-footer">Abhijith (©) </footer>
   </div>
 );
 
