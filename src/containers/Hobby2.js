708dce3b9b984fa1614d9c1f598d6f9011d50564
Abhijith <venu0005@algonquinlive.com>
2024-08-05 19:46:21 -0400

    done

--- src/containers/Hobby2.js
+++ src/containers/Hobby2.js
@@ -1,14 +1,17 @@
-import React from 'react'
-//
-import logoImg from '../'
+import React from 'react';
+import HobbyTemplate from '../templates/HobbyTemplate'; // Import the HobbyTemplate
 
-export default () => (
-  <div> 
-    <h1>Gaming</h1>
-    <p>Gaming is one of my favorite pastimes. It helps me unwind and I enjoy playing a variety of games across different genres.</p>
 
+const Hobby2 = () => (
+  <HobbyTemplate>
     <div>
-      <img src="/images/Games.jpg" alt="Photo 1" className="center" /> 
+      <h1>Gaming</h1>
+      <p>Gaming is one of my favorite pastimes. It helps me unwind and I enjoy playing a variety of games across different genres.</p>
+      <div>
+        <img src="/images/Games.jpg" alt="Gaming" className="center" />
+      </div>
     </div>
-  </div>
-)
+  </HobbyTemplate>
+);
+
+export default Hobby2;
