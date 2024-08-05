99b9c4ac95af596106077589baac7ddd7596ffe4
Abhijith <venu0005@algonquinlive.com>
2024-08-05 19:32:22 -0400

    d

--- src/containers/Hobby1.js
+++ src/containers/Hobby1.js
@@ -1,14 +1,14 @@
-import React from 'react'
-//
-import logoImg from '../'
+import React from 'react';
+import '../styles/Hobby1.css'; // Import your CSS file for styling
 
 export default () => (
-  <div> 
+  <div>
     <h1>Photography</h1>
     <p>I love capturing moments through my camera lens. Photography allows me to express my creativity and see the world from different perspectives.</p>
 
-    <div>
-      <img src='/images/Photography.JPG' alt="Photo" className="center" /> 
+    <div className="photo-gallery">
+      <img src='/images/Photography.JPG' alt="Photo" className="center" />
+      {/* Add more images as needed */}
     </div>
   </div>
-)
+);
