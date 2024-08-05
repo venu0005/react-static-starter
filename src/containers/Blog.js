70a02aa90e304426488597dec17071ce0e5b0c77
Abhijith <venu0005@algonquinlive.com>
2024-08-05 16:25:27 -0400

    done

--- src/containers/Blog.js
+++ src/containers/Blog.js
@@ -3,6 +3,6 @@ import React from 'react'
 export default () => (
   <div>
     <h1>Blog</h1>
-    <p>Read our latest articles and updates on this page.</p>
+    <p>Read our latest articles and Updates on this page.</p>
   </div>
 )
