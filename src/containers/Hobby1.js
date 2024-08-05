26281166d92ab87d7f6b477beadeed114996d4a1
Abhijith <venu0005@algonquinlive.com>
2024-08-05 19:36:31 -0400

    d

--- src/containers/Hobby1.js
+++ src/containers/Hobby1.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import '../styles/Hobby1.css'; // Import your CSS file for styling
+import HobbyTemplate from '../templates/HobbyTemplate'; // Import the HobbyTemplate
+//import '../styles/Hobby1.css'; // Import specific styles for the Hobby1 page
 
-export default () => (
-  <div>
-    <h1>Photography</h1>
-    <p>I love capturing moments through my camera lens. Photography allows me to express my creativity and see the world from different perspectives.</p>
-
-    <div className="photo-gallery">
-      <img src='/images/Photography.JPG' alt="Photo" className="center" />
-      {/* Add more images as needed */}
+const Hobby1 = () => (
+  <HobbyTemplate>
+    <div>
+      <h1>Photography</h1>
+      <p>I love capturing moments through my camera lens. Photography allows me to express my creativity and see the world from different perspectives.</p>
+      <div className="photo-gallery">
+        <img src='/images/Photography.JPG' alt="Photography" className="center" />
+        {/* Add more images if needed */}
+      </div>
     </div>
-  </div>
+  </HobbyTemplate>
 );
+
+export default Hobby1;
