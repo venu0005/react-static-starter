4c63369cb6424f96c7ab44a7150c2692b10d2960
Abhijith <venu0005@algonquinlive.com>
2024-08-05 20:06:06 -0400

    d

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Home.css'; // Import your CSS file
+import 'src\containers\styles\home.css'; // Import your CSS file
 
 const Home = () => (
   <div className="home-container">
